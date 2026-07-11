3ca013c74bebe4dbf97190277f87e675d7bcfce0
mohitmarathe99-ops <mohitmarathe0099@gmail.com>
2026-07-11 14:38:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.59%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revnum = 0;
+        
+        while (x != 0) {
+            int lastdigit = x % 10;
+            
+            // Check for overflow before multiplying by 10
+            if (revnum > INT_MAX / 10 || revnum < INT_MIN / 10) {
+                return 0;
+            }
+            
+            revnum = revnum * 10 + lastdigit;
+            x = x / 10;
+        }
+        
+        return revnum; // Return statement belongs outside the loop
+    }
+};
